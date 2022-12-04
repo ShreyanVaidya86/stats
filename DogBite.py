f73cc8562415803224d01c2225afaa7aa94ed54b
karenalicia14 <118768006+karenalicia14@users.noreply.github.com>
2022-12-03 20:44:49 -0500

    Update DogBite.py

--- DogBite.py
+++ DogBite.py
@@ -70,3 +70,65 @@ plt.xlabel("Season", labelpad=15)
 plt.ylabel("Number of Bites", labelpad=15)
 plt.title("Number of Bites by Season", y=1.02, fontsize=22)
 plt.show()
+
+#2. Which is the most aggressive dog breed?
+breed_mode = mydataset.loc[:,"Breed"].mode()          #Finding the mode in column 'Breed'
+print("The most aggressive breed is the ", breed_mode[0])
+top_10 = breed_df.tail(10)
+top_10.plot(x ='Breed', y='Bites by Breed', kind='barh')
+plt.xlabel("Breed", labelpad=15)
+plt.ylabel("Number of Bites", labelpad=15)
+plt.title("Number of Bites by Dog's Breed", y=1.02, fontsize=22)
+plt.show()
+
+#2.i. What is the most aggressive gender
+gender_mode = mydataset.loc[:,"Gender"].mode()
+if gender_mode[0] == "M":
+    print("Masculine dog gender is the most aggressive")
+elif gender_mode[0] == "F":
+    print("Feminine dog gender is the most aggressive")
+else:
+    print("The most aggressive gender is unknown")
+#Let's plot the information
+the_grid = GridSpec(2, 2)                                                   #Figure details
+plt.subplot(the_grid[0, 1], aspect=1, title='Dog Bites by Gender')
+gender_id = plt.pie(gender_counts, labels=gender_labels, autopct='%1.1f%%')
+plt.show()
+
+#2.ii. Which is the age at which dogs are more aggressive?
+age_mode = mydataset.loc[:,"Age"].mode()        #Finding the mode in 'Age' column
+print("At  ", age_mode[0], " years old, dogs become more aggressive.")
+#Normal Distribution for Age of dogs
+age_mean = mydataset['Age'].mean()
+age_variance = mydataset['Age'].var(ddof=1)
+age_sd = mydataset['Age'].std(ddof=1)
+mu_age = age_mean                               #mean
+sigma_age = age_sd                              #standard deviation
+s = np.random.normal(mu_age, sigma_age, 10000)
+#Let's plot the information
+count, bins, ignored = plt.hist(s, 30, density=True)
+plt.plot(bins, 1/(sigma_age * np.sqrt(2 * np.pi)) *
+               np.exp( - (bins - mu_age)**2 / (2 * sigma_age**2) ),
+         linewidth=2, color='r')
+plt.xlabel('Age of Dogs')                       #Title label
+plt.show()
+
+#2.iii. Do dogs with mixed breeds tend to bite more?
+cross_breed_mode = mydataset.loc[:,"CrossBreed"].mode()      #Finding the mode in column CrossBreed
+if cross_breed_mode[0] == True:
+    print("Mixed breed dogs definitely tend to bite more.")
+elif cross_breed_mode[0] == False:
+    print("Mixed breed dogs don't bite more than non-cross breed ones.")
+plt.subplot(the_grid[0, 1], aspect=1, title='Cross Breed Dog Bites')
+cross_breed_id = plt.pie(cross_breed_counts, labels=cross_breed_labels, autopct='%1.1f%%')
+plt.show()
+
+#3. Do neutered dogs bite less?
+spay_neuter_mode = mydataset.loc[:,"SpayNeuter"].mode()      #Finding the mode in column SpayNeuter
+if spay_neuter_mode[0] == True:
+    print("Neutered dogs bite more.")
+elif spay_neuter_mode[0] == False:
+    print("Neutered dogs don't bite more.")
+plt.subplot(the_grid[0, 1], aspect=1, title='Neutered Dog Bites')
+spay_neuter_id = plt.pie(spay_neuter_counts, labels=spay_neuter_labels, autopct='%1.1f%%')
+plt.show()
